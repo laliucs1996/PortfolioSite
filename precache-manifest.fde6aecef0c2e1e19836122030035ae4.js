38296835f3c9b02351cbe219506c123f3e404ddf
Larry <laliucs1996@gmail.com>
2019-07-30 19:45:23 -0400

    Updates

---
+++ precache-manifest.fde6aecef0c2e1e19836122030035ae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf9bb1586ef0b04c1bb0b2f64f82521",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "0254905f231f5b6fbfc4",
+    "url": "/repository-name/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5303d647319c3e392adf",
+    "url": "/repository-name/static/css/main.e4c430ce.chunk.css"
+  },
+  {
+    "revision": "0254905f231f5b6fbfc4",
+    "url": "/repository-name/static/js/2.a501e601.chunk.js"
+  },
+  {
+    "revision": "5303d647319c3e392adf",
+    "url": "/repository-name/static/js/main.c5bbc9b8.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "da386a91e8bd90fe38cf41844238e162",
+    "url": "/repository-name/static/media/Background.da386a91.jpg"
+  }
+]);
