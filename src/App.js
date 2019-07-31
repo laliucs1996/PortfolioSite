3dbd6b07731cb53040a10de372919968f79e7b96
Larry <laliucs1996@gmail.com>
2019-07-31 09:43:50 -0400

    Deploy Test

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Home from "./Home.js";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import AboutMe from "./AboutMe.js";
 import Resume from "./Resume.js";
 import Projects from "./Projects.js";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter baseName="/Larry">
       <Route path={"/"} exact component={Home} />
       <Route path={"/aboutMe"} component={AboutMe} />
       <Route path={"/resume"} component={Resume} />
       <Route path={"/projects"} component={Projects} />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
